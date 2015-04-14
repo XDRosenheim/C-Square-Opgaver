1f4560e5f15cc44cc4b45b7ccf32e37519795fad
Rosenheim <XDRosenheim@users.noreply.github.com>
2015-04-14 08:51:04 +0200

    First files.
    C# school lesson.

---
+++ Hello World/Hello World/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Fornavn: ");
+            string fornavn = Console.ReadLine();
+            Console.Write("Efternavn: ");
+            string efternavn = Console.ReadLine();
+            Console.WriteLine("Dit fulde navn: " + fornavn + " " + efternavn);
+            Console.ReadKey();
+        }
+    }
+}
